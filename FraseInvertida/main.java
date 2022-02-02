cb17d9dfe6d52348c8d20a87a7a4c555035f70c7
Unnamed <51212563+Meee3@users.noreply.github.com>
2022-02-02 17:51:15 -0300

    4 exercise- mero esboço

---
+++ FraseInvertida/main.java
@@ -0,0 +1,23 @@
+package FraseInvertida;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Digite uma frase");
+		
+		String valorEntrada = in.nextLine();
+		
+		//poderia inserir em uma collection list com indice e depois fazer outra e inverter e fazer igual os valores se batem...
+		// e validar se true or false
+		// não terei tempo para implementar
+		// mb
+		
+	}
+
+}
